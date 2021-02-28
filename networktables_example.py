f853d79a5da9706e47dc7e1cc18299f3d4ae0575
Glenn Buckholtz <buckholt@queensu.ca>
2021-02-28 17:22:39 -0500

    Passing data example - needs comments

--- networktables_example.py
+++ networktables_example.py
@@ -1,18 +1,34 @@
 import time
 from networktables import NetworkTables
+import json
 
 # To see messages from networktables, you must setup logging
 import logging
 
 logging.basicConfig(level=logging.DEBUG)
 
+#here is a named list of waypoints
+
+waypointList1 = [[.1,.1], [.2,.2], [.3,.3]]
+waypointList2 = [[3,5], [1,2],[4,5],[1,1],[7,3]]
+paths = {
+        "allPaths":[{
+            "pathname":"LoopDeLoop",
+            "waypoints": json.dumps(waypointList1)
+        }
+         ,
+         {
+         "pathname":"ZigZag",
+         "waypoints": json.dumps(waypointList2)
+         }]
+        }
+
+# we will eventually put the ip address of the robot here
 NetworkTables.initialize()
-sd = NetworkTables.getTable("SmartDashboard")
+sd = NetworkTables.getTable("SmartDashboard/ClickDrive") #we are writing to a subfolder of the SmartDashboard called ClickDrive
 
 i = 0
 while True:
-    print("dsTime:", sd.getNumber("dsTime", -1))
-
-    sd.putNumber("robotTime", i)
+    sd.putString("trajectories",json.dumps(paths))
     time.sleep(1)
     i += 1
